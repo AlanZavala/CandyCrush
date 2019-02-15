fb89677f830c54ab0d549f34cf9002d1ecf9aec0
chito0399 <37593552+chito0399@users.noreply.github.com>
2019-02-15 17:23:43 -0600

    Player2

---
+++ CandyActi/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private int ballsDestroyed = 0;
+
+    private player[][] matriz;
+
+    public player ball1;
+    public player ball2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        matriz = new player[8][];
+        matriz[0] = new player[7];
+
+
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                matriz[i][j] = new player();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void UpdateDestroyed()
+    {
+        ballsDestroyed += 3;
+    }
+
+    public void SelectedBall(player ball)
+    {
+        if(ball1 == null)
+        {
+            ball1 = ball;
+        }
+        else
+        {
+            ball2 = ball;
+            Search();
+        }
+    }
+
+    public void Search()
+    {
+
+    }
+}
--- CandyActi/Assets/Scripts/player.cs
+++ CandyActi/Assets/Scripts/player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+
+    public int color;
+    public GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(color == 1)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else if (color == 2)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnMouseDown()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.grey;
+        gm.SelectedBall(this);
     }
 }
