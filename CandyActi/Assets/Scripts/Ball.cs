1dddfc92b724cf4926f12bc31cc7bd5435205ae5
chito0399 <37593552+chito0399@users.noreply.github.com>
2019-02-19 12:49:21 -0600

    Paso2,3,4

--- CandyActi/Assets/Scripts/Ball.cs
+++ CandyActi/Assets/Scripts/Ball.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Ball : MonoBehaviour
 {
     public int color;
+    public int x;
+    public int y;
 
     public GameManager gm;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        ColorBall();        
+    }
 
-        if(color == 1 )
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCoordinates(int newx, int newy)
+    {
+        x = newx;
+        y = newy;
+    }
+
+    public void OnMouseDown()
+    {
+        //Debug.Log("hola");
+        gameObject.GetComponent<Renderer>().material.color = Color.grey;
+        gm.SelectedBall(this);
+    }
+
+    public void ColorBall()
+    {
+        if (color == 1)
         {
             gameObject.GetComponent<Renderer>().material.color = Color.yellow;
         }
@@ -25,18 +49,5 @@ public class player : MonoBehaviour
         {
             gameObject.GetComponent<Renderer>().material.color = Color.red;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnMouseDown()
-    {
-        Debug.Log("hola");
-        gameObject.GetComponent<Renderer>().material.color = Color.grey;
-//        gm.SelectedBall(this);
     }
 }
--- CandyActi/Assets/Scripts/GameManager.cs
+++ CandyActi/Assets/Scripts/GameManager.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    private int ballsDestroyed = 0;
-
     private GameObject[,] matriz;
+    public Ball fball;
+    public Ball sball;
+    public Ball aux; 
 
-    public player fball;
-    public player sball;
     private int k = 1;
+    private Vector2 velocity;
+    public float smoothTime;
+
+    private int ballsDestroyed = 0;
+    private bool canbeSwaped; //to verify if the ball can swap (they have to together)
+             
 
     // Start is called before the first frame update
     void Start()
@@ -24,32 +29,24 @@ public class GameManager : MonoBehaviour
         {
             for(int j=0; j<7; j++)
             {
-
-                matriz[i,j]=GameObject.Find("Ball" + k);
-
-                Debug.Log(matriz[i,j].gameObject.GetComponent<player>().color);
+                string look = "Ball" + k;
+                matriz[i,j]=GameObject.Find(look);
+                GameObject.Find(look).gameObject.GetComponent<Ball>().x = i;
+                GameObject.Find(look).gameObject.GetComponent<Ball>().y = j;
+                //Debug.Log(matriz[i,j].gameObject.GetComponent<Ball>().color);
                 k++;
-                Debug.Log(k);
+               // Debug.Log(k);
 
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     public void UpdateDestroyed()
     {
         ballsDestroyed += 3;
     }
 
-
-
-    public void SelectedBall(player ball)
+    public void SelectedBall(Ball ball)
     {
         if(fball == null)
         {
@@ -58,12 +55,88 @@ public class GameManager : MonoBehaviour
         else
         {
             sball = ball;
-            Change();
+            Verify();
+            if (canbeSwaped)
+            {
+                Swap();
+                Move();
+                canbeSwaped = false;
+            }
+            else
+            {
+                fball = null;
+                sball = null;
+            }
         }
     }
 
-    public void Change()
+    public void Verify()
     {
+        //Debug.Log("La x de la primer es: " + fball.x + " y la 'y' es: " + fball.y);
+        //Debug.Log("La x de la segunda es: " + sball.x + " y la 'y' es: " + sball.y);
 
+        //First check from right to left 
+        if (  ((fball.x +1) == sball.x )  || ((fball.x - 1) == sball.x)    )
+        {
+            //WE HAVE TO CHANGE THE X
+            //The check from up and down
+            if(fball.y == sball.y)
+            {
+                Debug.Log("Can be swaped");
+                canbeSwaped = true;
+            }                            
+        }
+
+        //First check from right to left 
+        if (   ((fball.y + 1) == sball.y) || ((fball.y - 1) == sball.y))
+        {
+            //WE HAVE TO CHANGE THE X
+            //The check from up and down
+            if (fball.x == sball.x)
+            {
+                Debug.Log("Can be swaped");
+                canbeSwaped = true;
+            }
+        }
+        fball.ColorBall();
+        sball.ColorBall();
     }
+
+    public void Swap()
+    {
+        //THIS METHOD CHANGE THE BALL IN THE MATRIX
+        int xaux = fball.x;
+        int yaux = fball.y;
+        string nameaux = fball.name;
+        //Debug.Log("La x de la primer es: " + fball.x + " y la 'y' es: " + fball.y);
+        //Debug.Log("La x de la segunda es: " + sball.x + " y la 'y' es: " + sball.y);
+
+        fball.x = sball.x;
+        fball.y = sball.y;
+        fball.name = sball.name;
+
+        sball.x = xaux;
+        sball.y = yaux;
+        sball.name = nameaux;
+
+        //Debug.Log("La x de la primer es: " + fball.x + " y la 'y' es: " + fball.y);
+        //Debug.Log("La x de la segunda es: " + sball.x + " y la 'y' es: " + sball.y);
+    }
+
+    public void Move()
+    {
+        float xaux = fball.transform.position.x;
+        float yaux = fball.transform.position.y;
+        float zaux = fball.transform.position.z;
+
+        //aux.transform.position = fball.transform.position;
+
+        fball.transform.position = sball.transform.position;
+        sball.transform.position = new Vector3(xaux, yaux, zaux);
+
+
+        fball = null;
+        sball = null;
+    }
+
 }
