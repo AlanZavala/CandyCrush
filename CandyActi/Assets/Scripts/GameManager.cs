46f419660e389de845fbe972ee7b08c33ee866a5
chito0399 <37593552+chito0399@users.noreply.github.com>
2019-02-20 17:56:45 -0600

    Primer avance del paso 5

--- CandyActi/Assets/Scripts/GameManager.cs
+++ CandyActi/Assets/Scripts/GameManager.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 
-    private GameObject[,] matriz;
+    private int [,] matriz;
+    private GameObject[,] auxMatrix;
     public Ball fball;
     public Ball sball;
-    public Ball aux; 
+    LinkedList<GameObject> rows = new LinkedList<GameObject>(); //This list will store the ball that will be destroyed. 
 
     private int k = 1;
     private Vector2 velocity;
@@ -23,7 +24,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        matriz = new GameObject[7,7];
+        matriz = new int [7,7];
+        auxMatrix = new GameObject[7, 7];
         brokenText.text = "Piezas eliminadas: " + ballsDestroyed;
 
 
@@ -32,13 +34,11 @@ public class GameManager : MonoBehaviour
             for(int j=0; j<7; j++)
             {
                 string look = "Ball" + k;
-                matriz[i,j]=GameObject.Find(look);
+                auxMatrix[i, j] = GameObject.Find(look);
+                matriz[i, j] = GameObject.Find(look).gameObject.GetComponent<Ball>().color;
                 GameObject.Find(look).gameObject.GetComponent<Ball>().x = i;
                 GameObject.Find(look).gameObject.GetComponent<Ball>().y = j;
-                //Debug.Log(matriz[i,j].gameObject.GetComponent<Ball>().color);
-                k++;
-               // Debug.Log(k);
-
+                k++;              
             }
         }
     }
@@ -75,7 +75,7 @@ public class GameManager : MonoBehaviour
 
     public void Verify()
     {
-        //First check from right to left 
+        //First check from right to left       
         if (  ((fball.x +1) == sball.x )  || ((fball.x - 1) == sball.x)    )
         {            
             //The check from up and down
@@ -100,22 +100,42 @@ public class GameManager : MonoBehaviour
         sball.ColorBall();
     }
 
-    public void Swap()
+    public void Swap() //THIS METHOD CHANGE THE VALUES OF THE MATRIX  -> 'CHANGE THE COLOR OF THE BALL'
     {
-        //THIS METHOD CHANGE THE BALL IN THE MATRIX
+        //keep an aux to store the value of the first ball.
+        int auxColor = fball.gameObject.GetComponent<Ball>().color;
+
+        int auxX1 = fball.gameObject.GetComponent<Ball>().x;
+        int auxY1 = fball.gameObject.GetComponent<Ball>().y;
+
+        int auxX2 = sball.gameObject.GetComponent<Ball>().x;
+        int auxY2 = sball.gameObject.GetComponent<Ball>().y;
+
+        matriz[auxX1, auxY1] = matriz[auxX2, auxY2];
+        matriz[auxX2, auxY2] = auxColor;
+
+        fball.gameObject.GetComponent<Ball>().x = auxX2;
+        fball.gameObject.GetComponent<Ball>().y = auxY2;
+
+        sball.gameObject.GetComponent<Ball>().x = auxX1;
+        sball.gameObject.GetComponent<Ball>().y = auxY1;
+
+
+        /*GameObject aux = fball.gameObject;
         int xaux = fball.x;
         int yaux = fball.y;
         string nameaux = fball.name;
 
+        matriz[xaux, yaux] = matriz[sball.x, sball.y];
+        matriz[sball.x, sball.y] = aux;
+              
         fball.x = sball.x;
         fball.y = sball.y;
         fball.name = sball.name;
 
         sball.x = xaux;
         sball.y = yaux;
-        sball.name = nameaux;
-
-        //FATA CAMBIARLO REALMENTE EN LA MATRIZ
+        sball.name = nameaux;*/
 
     }
 
@@ -129,94 +149,130 @@ public class GameManager : MonoBehaviour
 
         fball.transform.position = sball.transform.position;
         sball.transform.position = new Vector3(xaux, yaux, zaux);
+
+        bool check = Check(fball);
+        if (check)
+        {
+            rows.AddFirst(FindTheBall(sball.gameObject.GetComponent<Ball>().x, sball.gameObject.GetComponent<Ball>().y));
+            DestroyBalls();
+            Debug.Log("la bola 1 sí tiene");
+        }
+        else
+        {            
+            Debug.Log("la bola 1 no tiene");
+            check = Check(sball);
+            if (check)
+            {
+                rows.AddFirst(FindTheBall(fball.gameObject.GetComponent<Ball>().x, fball.gameObject.GetComponent<Ball>().y));
+                DestroyBalls();
+                Debug.Log("la bola 2 sí tiene");
+            }
+            else
+            {
+                Debug.Log("la bola 2 no tiene");
+            }
+        }
+
+        
+        fball = null;
+        sball = null;
     }
 
-    public bool Check(Ball checkBall)
+    public bool Check(Ball checkBall) //This method check if there are three blocks together
     {
-        //This method check if there are three blocks together
         int x = checkBall.x;
         int y = checkBall.y;
         int pivote = 1;
-
         int together = 0;
-
         int color = checkBall.gameObject.GetComponent<Ball>().color;
 
-        bool can = false;
-
-        while (x<6 && together<2)
+        while (x+pivote<=6 && together<2)
         {
-            if(matriz[x+pivote,y].gameObject.GetComponent<Ball>().color != color)
-            {
-                while (x > 0 && together < 2)
-                {
-                    pivote = 1;
-                    if (matriz[x-pivote, y].gameObject.GetComponent<Ball>().color != color)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        together++;
-                        pivote--;
-                    }
-                }
+            if(matriz[x+pivote,y] != color)
+            {                
                 break;
             }
             else
             {
+                rows.AddLast(FindTheBall(x + pivote, y));
                 together++;
-                pivote++;
+                pivote++;                
+            }            
+        }
+        pivote = 1;
+        while (x - pivote >= 0 && together < 2)
+        {            
+            if (matriz[x-pivote, y] != color)
+            {
+                break;
             }
-            
+            else
+            {
+                rows.AddLast(FindTheBall(x - pivote, y));
+                together++;
+                pivote--;                
+            }
         }
 
-        if (together == 2)
+        if (together != 2)
         {
-            can = true;
-        }
-        else
-        {
+            rows.Clear();
             together = 0;
-            while (y < 6 && together < 2)
-            {
-                if (matriz[x, y+pivote].gameObject.GetComponent<Ball>().color != color)
+            pivote = 1;
+            while (y + pivote <= 6 && together < 2)
+            {                
+                if (matriz[x, y + pivote] != color)
                 {
-                    while (y > 0 && together < 2)
-                    {
-                        pivote = 1;
-                        if (matriz[x, y-pivote].gameObject.GetComponent<Ball>().color != color)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            together++;
-                            pivote--;
-                        }
-                    }
                     break;
                 }
                 else
                 {
+                    rows.AddLast(FindTheBall(x, y+pivote));
                     together++;
                     pivote++;
                 }
 
             }
-            if (together == 2)
+            pivote = 1;
+            while (y - pivote >= 0 && together < 2)
             {
-                can = true;
+                if (matriz[x, y - pivote] != color)
+                {
+                    break;
+                }
+                else
+                {
+                    rows.AddLast(FindTheBall(x, y - pivote));
+                    together++;
+                    pivote--;
+                }
             }
-        }
-        
+        }       
 
-        return can;
+        if (together == 2)
+        {
+            return true;
+        }
+        else
+        {
+            rows.Clear();
+            return false;
+        }                   
     }
 
     public void DestroyBalls()
     {
-        
+        UpdateDestroyed();
+        for(int d=0; d<3; d++)
+        {
+            Destroy(rows.Last.Value);
+            rows.RemoveLast();
+        }
+    }
+
+    public GameObject FindTheBall(int x, int y)
+    {
+        return auxMatrix[x, y];
     }
 
 }
